024411f00acc58154fea9046e1cb1a5ac9805da8
Paul Dijou <paul.dijou+dev@gmail.com>
2013-10-24 11:56:58 +0200

    Update _=prompts.name_s.scala

--- app/controllers/_=prompts.name_s.scala
+++ app/controllers/_=prompts.name_s.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 import play.api._
@@ -9,6 +10,7 @@ import play.api.Play.current
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
+import play.modules.reactivemongo.ReactiveMongoPlugin
 import play.modules.reactivemongo.json.collection.JSONCollection
 import play.autosource.reactivemongo._
 
